00cc7c56b1b9deeb13797983a5ac70af1ea20fac
benyaminsoldier <daniel.benjumea0@gmail.com>
2025-04-03 13:11:36 -0600

    initial commit with jenkins

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Daniel Benjumea/i);
   expect(linkElement).toBeInTheDocument();
 });
