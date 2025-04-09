a7dd04deff47c33bf27cccfefee1217f8ecefff9
christineR745 <c.ritchie745@mybvc.ca>
2025-04-08 19:33:30 -0600

    Added name

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Daniel Benjumea V</p>
+        <p>Christine Ritchie</p>
         <a
           className="App-link"
           href="https://reactjs.org"
